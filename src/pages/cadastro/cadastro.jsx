36c189f56b8c07cdd4a2107a6b0764e8c623e044
Beatriz Pinheiro <beatrizpinheiro2001@gmail.com>
2025-06-14 17:54:33 -0300

    css da pagina de cadastro e link para ir direto para o login quando finalizar o cadastro com sucesso

--- src/pages/cadastro/cadastro.jsx
+++ src/pages/cadastro/cadastro.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import apiUsuarios from "../../services/apiUsuarios";
-import { Input } from "../../components/Input/Input";
 import styles from "./cadastro.module.css";
 import { Navbar } from "../../components/navbar/navbar";
 import { Footer } from "../../components/footer/footer";
+import perfil from "../../assets/perfil.png"
+import { useNavigate } from "react-router-dom";
 
 export function CadastroPage() {
+    const navigate = useNavigate()
+
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPass, setConfirmPass] = useState("");
@@ -13,6 +16,7 @@ export function CadastroPage() {
     const [cpf, setCpf] = useState("");
     const [phone, setPhone] = useState("");
     const [address, setAddress] = useState("");
+
     const postUsuario = async () => {
         const user = {
             name,
@@ -30,6 +34,7 @@ export function CadastroPage() {
             const response = await apiUsuarios.post("/users", user);
             console.log("Usuário cadastrado", response.data);
             alert("Usuário cadastrado com sucesso!");
+            navigate("/login")
         } catch (erro) {
             console.error("Erro ao cadastrar:", erro);
             alert("Erro ao cadastrar usuário.");
@@ -42,38 +47,45 @@ export function CadastroPage() {
     return (
         <div className={styles.cadastro}>
             <Navbar></Navbar>
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <label>Nome:
-                    <Input type="text" placeholder="Nome" value={name} onChange={(e) =>
-                        setName(e.target.value)} />
-                </label>
-                <label>Senha:
-                    <Input type="password" placeholder="Senha" value={password} onChange={(e) =>
-                        setPassword(e.target.value)} />
-                </label>
-                <label>Confirmar Senha:
-                    <Input type="password" placeholder="Confirmar Senha" value={confirmPass}
-                        onChange={(e) => setConfirmPass(e.target.value)} />
-                </label>
-                <label>Email:
-                    <Input type="email" placeholder="Email" value={email} onChange={(e) =>
-                        setEmail(e.target.value)} />
-                </label>
-                <label>CPF:
-                    <Input type="text" placeholder="CPF" value={cpf} onChange={(e) =>
-                        setCpf(e.target.value)} />
-                </label>
-                <label>Telefone:
-                    <Input type="text" placeholder="Telefone" value={phone} onChange={(e) =>
-                        setPhone(e.target.value)} />
-                </label>
-                <label>Endereço:
-                    <Input type="text" placeholder="Endereço" value={address} onChange={(e) =>
-                        setAddress(e.target.value)} />
-                </label>
-                <button type="submit">Cadastrar</button>
-            </form>
-             <Footer></Footer>
+            <div className={styles.box}>
+                <div className={styles.divForm}>
+                    <img src={perfil} alt="perfil" />
+                    <h2>Cadastrar Nova Conta</h2>
+                    <form onSubmit={handleSubmit}>
+                        <label>Nome</label>
+                        <input type="text" placeholder="Digite seu nome" value={name} onChange={(e) =>
+                            setName(e.target.value)} />
+                        <label>E-mail</label>
+                        <input type="email" placeholder="Digite seu e-mail" value={email} onChange={(e) =>
+                            setEmail(e.target.value)} />
+
+                        <label>CPF</label>
+                        <input type="text" placeholder="Digite seu CPF" value={cpf} onChange={(e) =>
+                            setCpf(e.target.value)} />
+
+                        <label>Telefone</label>
+                        <input type="text" placeholder="Digite seu telefone" value={phone} onChange={(e) =>
+                            setPhone(e.target.value)} />
+
+                        <label>Endereço</label>
+                        <input type="text" placeholder="Digite seu endereço" value={address} onChange={(e) =>
+                            setAddress(e.target.value)} />
+
+                        <label>Senha</label>
+                        <input type="password" placeholder="Digite sua senha" value={password} onChange={(e) =>
+                            setPassword(e.target.value)} />
+
+                        <label>Confirmar Senha</label>
+                        <input type="password" placeholder="Confirme sua senha" value={confirmPass}
+                            onChange={(e) => setConfirmPass(e.target.value)} />
+
+                        <div className={styles.divButton}>
+                            <button type="submit">Cadastrar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <Footer></Footer>
         </div>
     );
 }
